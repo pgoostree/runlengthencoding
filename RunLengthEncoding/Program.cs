85f1d62b6b06bd9533146b1ba6494bff49805727
Peter Goostree <pgoostree@cylance.com>
2020-04-09 21:00:01 -0700

    initial add

---
+++ RunLengthEncoding/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace PeterTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = EncodeString("aabccd");
+            Console.WriteLine(result);
+
+            var result1 = EncodeString1("aabccd");
+            Console.WriteLine(result1);
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Encodes a string using RunLength encoding algorithm
+        ///
+        /// Example: Given the following input:
+        ///     aaabbbccddda
+        /// The output would be:
+        ///     3a2b2c3d1a
+        /// 
+        /// </summary>
+        /// <param name="input">A string to be encoded</param>
+        /// <returns>A RunLength encoded string</returns>
+        private static string EncodeString(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return "";
+            
+            StringBuilder sb = new StringBuilder();
+            var inputChars = input.ToCharArray();
+
+            var index = 0;
+
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                if (inputChars[i] != inputChars[index])
+                {
+                    sb.Append(i - index);
+                    sb.Append(inputChars[index]);
+                    index = i;
+                }
+            }
+            
+            sb.Append(inputChars.Length - index);
+            sb.Append(inputChars[index]);
+            
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Encodes a string using RunLength encoding algorithm
+        ///
+        /// Example: Given the following input:
+        ///     aaabbbccddda
+        /// The output would be:
+        ///     3a2b2c3d1a
+        /// 
+        /// </summary>
+        /// <param name="input">A string to be encoded</param>
+        /// <returns>A RunLength encoded string</returns>
+        private static string EncodeString1(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            var group = input.GroupBy(x => x);
+
+            foreach (var item in group)
+            {
+                sb.AppendFormat("{0}{1}", item.Count(), item.Key);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
